d17a87f440efc116e2ddff735be6d7cebc05c145
SirTapir <37269469+SirTapir@users.noreply.github.com>
2019-05-20 21:17:43 +0700

    update viewProduct in supplier

--- supplier/viewProduct.php
+++ supplier/viewProduct.php
@@ -6,6 +6,14 @@
 	if(!isset($_SESSION["user-supplier"])){ //if login in session is not set
     	header("Location: ../login.php");
 	}
+
+
+	$sql2 = "SELECT * FROM user_supplier WHERE id=".$_SESSION["user-supplier"];
+	$supplierArray = $conn->query($sql2);
+	while($supplierName = mysqli_fetch_assoc($supplierArray)) :
+		$current_name = $supplierName['nama_supplier'];
+	endwhile;
+
 ?>
 
 
@@ -17,17 +25,18 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 </head>
 <body>
-	<h1 style="margin-left:600px;margin-top: 25px;">Data Product</h1>
+	<?php
+		echo '<h1 style="margin-top: 25px; align-content:center; text-align:center;">Data Product '.$current_name.'</h1>';
+	?>
+	
 	<table class="table table-striped" style="margin-top: 25px;">
 		<thead>
 			<tr>
 				<th scope="col">Id</th>
 				<th scope="col">Title</th>
 				<th scope="col">Price</th>
-				<th scope="col">Brand</th>
 				<th scope="col">Description</th>
 				<th scope="col">Feature</th>
-				<th scope="col">Color</th>
 				<th scope="col">Image</th>
 				<th scope="col">Edit</th>
 				<th scope="col">Delete</th>
@@ -36,7 +45,7 @@
 		<tbody>
 			<?php
 			#SQL get products
-			$query = "SELECT * FROM products";
+			$query = "SELECT * FROM products WHERE supplier_id=".$_SESSION["user-supplier"];;
 			$q = mysqli_query($conn,$query) or die (mysql_error($conn));
 
 
@@ -47,32 +56,22 @@
 				$id = $row["id"];
 				$title = $row["title"];
 				$price = $row["price"];
-				$brand = $row["brand"];
 				$desc = $row["description"];
 				$featured = $row["featured"];
-				$color = $row["color"];
 				$img =$row["image"];
 				$imgShow = ".".$img;
-
-				#SQL get brands name
-				$sql2 = "SELECT * FROM brands WHERE id=".$brand;
-				$brandArray = $conn->query($sql2);
 				
-
-			
-
+				
 				echo"<tr>";
 					echo "<td>$id</td>";
 					echo "<td>$title</td>";
 					echo "<td>$price</td>";
 					
 					
-					while($brandName = mysqli_fetch_assoc($brandArray)) :
-							echo "<td>".$brandName['brand']."</td>";
-					endwhile;
+					
 					echo "<td>$desc</td>";
 					echo "<td>$featured</td>";
-					echo "<td>$color</td>";
+					
 					echo "<td><a href = '$imgShow'>Click here</a></td>";
 					echo "<td><a href='editProducts.php?id=$id' style='color: black;'><button type='button' class='btn btn-warning'><i class='fa fa-cog'></i></button></a></td>";
 					echo "<td><button type='button' class='btn btn-danger' onclick='deleteItem($id)'><i class='fa fa-close'></i></button></td>";
